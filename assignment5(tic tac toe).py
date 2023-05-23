71ad32fba564aa10a6d486d7c8e4ac6186939c6f
mahbubehzarkesh <132184344+mahbubehzarkesh@users.noreply.github.com>
2023-05-23 09:53:40 +0330

    Update assignment5(tic tac toe).py

--- assignment5(tic tac toe).py
+++ assignment5(tic tac toe).py
@@ -1,48 +1,71 @@
-
+from pyfiglet import Figlet
+from termcolor import colored
+f=Figlet(font='banner3-D')
+print(colored(f.renderText('TIC  TAC  TOE'),'light_yellow'))
+                            
 def show():
+     
+     
+
      for row in game_board :
       for cell in row:
         print(cell, end="")
       print ()   
+
+def show_winner1():
+    from pyfiglet import Figlet
+    from termcolor import colored
+    f=Figlet(font='slant')
+    print(colored(f.renderText('congratulation player1 ... you win !!'),'light_red'))
+    return False
+def show_winner2():
+    from pyfiglet import Figlet
+    from termcolor import colored
+    f=Figlet(font='slant')
+    print(colored(f.renderText('congratulation player1 ... you win !!'),'light_red')) 
+
 def check_win_player1():
      
       if game_board[0][0]=="x" and game_board[0][1]=="x" and game_board[0][2]=="x":
-          print("player1 wins")
+          show_winner1()
          
       if game_board[1][0]=="x" and game_board[1][1]=="x" and game_board[1][2]=="x":
-          print("player1 wins")
+          show_winner1()
       
       if game_board[2][0]=="x" and game_board[2][1]=="x" and game_board[2][2]=="x":
-          print("player1 wins")
+         show_winner1()
+
       if game_board[0][0]=="x" and game_board[1][0]=="x" and game_board[2][0]=="x":
-          print("player1 wins")
+        show_winner1()
+
       if game_board[0][1]=="x" and game_board[1][1]=="x" and game_board[2][1]=="x":
-           print("player1 wins")
+           show_winner1()
+
       if game_board[0][0]=="x" and game_board[1][1]=="x" and game_board[2][2]=="x":
-           print("player1 wins")
-           return 1
+           show_winner1()
+
       if  game_board[0][2]=="x" and game_board[1][1]=="x" and game_board[2][0]=="x":
-           print("player1 wins")
+           show_winner1()
 
           
 def check_win_player2():
      
       if game_board[0][0]=="O" and game_board[0][1]=="O" and game_board[0][2]=="O":
-          print("player2 wins")
+          show_winner2()
           
       if game_board[1][0]=="O" and game_board[1][1]=="O" and game_board[1][2]=="O":
-          print("player2 wins")
+           show_winner2()
       if game_board[2][0]=="O" and game_board[2][1]=="O" and game_board[2][2]=="O":
-          print("player2 wins")
+            show_winner2()
       if game_board[0][0]=="O" and game_board[1][0]=="O" and game_board[2][0]=="O":
-          print("player2 wins")
+           show_winner2()
         
       if game_board[0][1]=="O" and game_board[1][1]=="O" and game_board[2][1]=="O":
-           print("player2 wins")
+             show_winner2()
       if game_board[0][0]=="O" and game_board[1][1]=="O" and game_board[2][2]=="O":
-           print("player2 wins")
+             show_winner2()
       if  game_board[0][2]=="O" and game_board[1][1]=="O" and game_board[2][0]=="O":
-           print("player2 wins")  
+             show_winner2()
                
 game_board=[["-","-","-"],
             ["-","-","-"],
@@ -51,7 +74,9 @@ game_board=[["-","-","-"],
 show()
 
 while True:
-  print("player1")
+  
+  print('player1 it is your turn . move')
+
  
   while True:
     row=int(input("enter row:"))
@@ -62,9 +87,9 @@ while True:
          game_board[row][col]="x"
          break
       else:
-       print("select another cell")   
+       print("!!! select another cell")   
     else:
-      print("select in 0,2")
+      print("!!!please select rows and columns in range  in 0,2")
   show()
   check_win_player1()
 
@@ -72,9 +97,9 @@ while True:
     
 # ////////////////////////////////
     
-  print("player2")
-  if check_win_player1()!=1 and check_win_player2!=1:
-      while True:
+  print("player2 it is your turn . move")
+  
+  while True:
               row=int(input("enter row:"))
               col=int(input("enter col:"))
               if 0<=row<=2 and 0<=col<=2 :
@@ -83,10 +108,10 @@ while True:
                   game_board[row][col]="O"
                   break
                 else:
-                  print("select another cell")   
+                  print("!!!! select another cell")   
               else:
-                print("select in 0,2")
+                print("!!!please select rows and columns in range  in 0,2")
 
-      show()
-      check_win_player2()
+  show()
+  check_win_player2()
     
